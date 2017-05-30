2b0a2dbe85b2be44988a0883029a5f5705dc2ad1
eurekaqq <npes89033@gmail.com>
2017-05-30 16:16:38 +0800

    first commit

--- code/code.py
+++ code/code.py
@@ -1,11 +1,25 @@
-# Big Data Analytics SecondHomework
+import pandas as pd
+import numpy as np
+import xgboost as xgb
+from xgboost.sklearn import XGBClassifier
+from sklearn import cross_validation, metrics
+from sklearn.grid_search import GridSearchCV
+ 
+import matplotlib.pylab as plt
+%matplotlib inline
+ 
+from matplotlib.pylab import rcParams
+rcParams['figure.figsize'] = 16, 4
 
-本次作業主要目的在於藉由sklearn的GridSearchCV測試怎樣的參數可以在xgboost得到最好的分類效果。
+data = pd.read_csv('./dataset/LargeTrain.csv')
+msk = np.random.rand(len(ndata)) < 0.8 
+train = ndata[msk]
+test = ndata[~msk]
 
----
+target='Class'
+predictors = [x for x in train.columns if x not in [target]]
 
-## (1) 首先先測試'max_depth'和'min_child_weight'的參數
-```python=
+#test paramet
 param_test1 = {
     'max_depth':list(range(3,10,2)),
     'min_child_weight':list(range(1,6,2))
@@ -15,12 +29,7 @@ gsearch1 = GridSearchCV(estimator = XGBClassifier( learning_rate =0.1, n_estimat
                                         objective= 'binary:logistic', nthread=4, scale_pos_weight=1, seed=27), 
                        param_grid = param_test1, scoring='roc_auc',n_jobs=4,iid=False, cv=5)
 gsearch1.fit(train[predictors],train[target])
-```
 
-得到最好的參數為```max_depth = 5，min_child_weight = 1```
-
-## (2) 使用(1)的結果修改參數後，接著測試'gamma'參數
-```python=
 param_test2 = {
     'gamma':list([i/10.0 for i in range(0,10)])
 }
@@ -29,12 +38,7 @@ gsearch2 = GridSearchCV(estimator = XGBClassifier( learning_rate =0.1, n_estimat
                                         objective= 'binary:logistic', nthread=4, scale_pos_weight=1, seed=27), 
                        param_grid = param_test2, scoring='roc_auc',n_jobs=4,iid=False, cv=5)
 gsearch2.fit(train[predictors],train[target])
-```
 
-得到最好的參數為```'gamma' = 0.0```
-
-## (3) 延續(2)的結果，tune'subsample'與'colsample_bytree'參數
-```python=
 param_test3 = {
      'subsample':list([i/10.0 for i in range(6,10)]),
      'colsample_bytree':list([i/10.0 for i in range(6,10)])
@@ -44,12 +48,7 @@ gsearch3 = GridSearchCV(estimator = XGBClassifier( learning_rate =0.1, n_estimat
                                         objective= 'binary:logistic', nthread=4, scale_pos_weight=1, seed=27), 
                        param_grid = param_test3, scoring='roc_auc',n_jobs=4,iid=False, cv=5)
 gsearch3.fit(train[predictors],train[target])
-```
 
-得到最好的```subsample = 0.8，colsample_bytree = 0.8。```
-
-## (4) 最後是'reg_alpha'
-```python=
 param_test4 = {
     'reg_alpha':[1e-5, 1e-2, 0.1, 1, 100]
 }
@@ -58,15 +57,8 @@ gsearch4 = GridSearchCV(estimator = XGBClassifier( learning_rate =0.1, n_estimat
                                         objective= 'binary:logistic', nthread=4, scale_pos_weight=1, seed=27), 
                        param_grid = param_test4, scoring='roc_auc',n_jobs=4,iid=False, cv=5)
 gsearch4.fit(train[predictors],train[target])
-```
 
-得到最好的```reg_alpha = 1e-05。```
-
-
-經過這一連串的測試後可以使用剛剛產生的參數，可以得到最好的預測率。
-
-### 最後再使用confusion matrix 驗證分析結果
-```python=
+#confusion_matrix
 import itertools
 import pandas as pd
 import numpy as np
@@ -130,13 +122,3 @@ plot_confusion_matrix(cnf_matrix, classes=class_names,
                       title='Confusion matrix, without normalization')
 
 plt.show()
-```
-![confusion matrix](./picture/confusion matrix.png)
-
----
-
-### Reference
-1. [XGBoostのパラメータチューニング実践 with Python](http://kamonohashiperry.com/archives/500)
-2. [Complete Guide to Parameter Tuning in Gradient Boosting (GBM) in Python](https://www.analyticsvidhya.com/blog/2016/02/complete-guide-parameter-tuning-gradient-boosting-gbm-python/)
-3. [Complete Guide to Parameter Tuning in XGBoost (with codes in Python)](https://www.analyticsvidhya.com/blog/2016/03/complete-guide-parameter-tuning-xgboost-with-codes-python/)
-4. [Confusion matrix](http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html#sphx-glr-auto-examples-model-selection-plot-confusion-matrix-py)
